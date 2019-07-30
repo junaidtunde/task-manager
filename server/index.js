1fe4724c2df0695ca0982ccffed38848ea8ff964
Junaid Babatunde <junaidtunde1@gmail.com>
2019-07-30 17:45:49 +0100

    removed node cron

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import app from './app';
 import http from 'http';
-import cron from 'node-cron';
-import db from '../models';
 
 var io = require('socket.io');
 
@@ -9,24 +7,6 @@ let server = http.createServer(app);
 var io = io.listen(server);
 
 const port = process.env.PORT || 8080;
-
-cron.schedule('0 * * * *', () => {
-    db.Centers.find().then(allCenters => {
-        if (allCenters === null) {
-            return;
-        } else {
-            allCenters.forEach(element => {
-                // console.log('here');
-                if (element.paid === true) {
-                    if (Date.now() > element.payment_exp) {
-                        element.paid = false;
-                        element.save();
-                    }
-                }
-            });
-        }
-    });
-});
 
 app.listen(port, (err) => {
     if (err) {
